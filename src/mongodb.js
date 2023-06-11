6296e55f264d0eabebc24995b4283152874ed92d
Anshul Rana <anshulrana2510@gmail.com>
2023-06-11 14:01:28 +0530

    Login signup complete

---
+++ src/mongodb.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/LoginSignUp")
+
+  .then(() => {
+    console.log("mongodb connected");
+  })
+
+  .catch(() => {
+    console.log("Failed to connect");
+  });
+
+const LogInSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const collection = new mongoose.model("collection1", LogInSchema);
+
+module.exports = collection; // exporting the collection to index.js
